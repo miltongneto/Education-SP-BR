69648849fd603698d865f222ef1736decca5e9d7
Milton Gama Neto <mvgn@cin.ufpe.br>
2020-05-03 18:46:41 -0300

    Improvements in interpretability. Refactoring and some adjustments in notebooks

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -38,3 +38,48 @@ def plot_kde_two_samples(df, attribute, bw='scott', figsize=(12, 7)):
     
     plt.xlabel(attribute, fontsize=14) 
     plt.ylabel('Densidade de Probabilidade', fontsize=14)
+
+
+def plot_scatter(df, x, y, figsize=(12, 10)) :
+    df_bom = df[df['DESEMPENHO'] == 1]
+    df_ruim = df[df['DESEMPENHO'] == 0]
+    
+    fig, ax = plt.subplots(figsize=figsize)
+
+    df_ruim.plot(kind='scatter', x=x, y=y, c='red', s=40, alpha=0.6, ax=ax, label='RUIM')
+    df_bom.plot(kind='scatter', x=x, y=y, c='blue', s=40, alpha=0.6, ax=ax, label='BOM')
+    
+    ax.set_title('Distribuicao dos dados', fontsize=14)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    
+    ax.tick_params(axis='x', labelsize=12)
+    ax.tick_params(axis='y', labelsize=12)
+    
+    plt.xlabel(x, fontsize=14) 
+    plt.ylabel(y, fontsize=14) 
+
+
+def plot_density_2d(df, x, y, figsize=(12, 10)) :
+    df_bom = df[df['DESEMPENHO'] == 1]
+    df_ruim = df[df['DESEMPENHO'] == 0]
+    
+    fig, ax = plt.subplots(figsize=figsize)
+
+    ax = sns.kdeplot(df_bom[x], df_bom[y],
+                 cmap="Blues", shade=True, shade_lowest=False, alpha=0.6, label='BOM')
+
+    ax = sns.kdeplot(df_ruim[x], df_ruim[y],
+                 cmap="Reds", shade=True, shade_lowest=False, alpha=0.6, label='RUIM')
+    
+    ax.set_title('Distribuicao dos dados', fontsize=14)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    
+    ax.tick_params(axis='x', labelsize=12)
+    ax.tick_params(axis='y', labelsize=12)
+    
+    plt.xlabel(x, fontsize=14) 
+    plt.ylabel(y, fontsize=14) 
+    
+    plt.legend()
